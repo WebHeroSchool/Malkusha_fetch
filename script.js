a2ca380bac8de192c64a87109ca47c042e0df688
Nastya <malkusha@mail.ru>
2020-10-05 23:25:23 +0300

    add

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 let url = 'https://api.github.com/users/malkusha';
-let info;
-let content;
-let avatar;
-let name;
-let bio;
-let page;
 async function getInfo() {
   let info = await fetch(url);
   let content = await info.json();
@@ -14,7 +8,6 @@ async function getInfo() {
   if (info.status !== 200) {
     title.innerHTML = 'Информация о пользователе не доступна';
   } else {
-
       let avatar = content.avatar_url;
       let name = content.name;
       let bio = content.bio;
