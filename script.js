9c0c79eda1da7670fe0e3e9f58440528dcaaf7ea
Nastya <malkusha@mail.ru>
2020-10-10 13:16:19 +0300

    style guide

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 let url = 'https://api.github.com/users/malkusha';
-let body = document.getElementsByTagName('body');
 let time = document.createElement('p');
 time.className = 'time';
 document.body.prepend(time);
-let preload = document.getElementById('preloader')
 let today = new Promise((resolve, reject) => {
   setTimeout(() => resolve(time.innerHTML = new Date), 2000);
 });
@@ -13,27 +11,31 @@ let acc = new Promise ((resolve, reject) => {
 async function getInfo() {
   let info = await fetch(url);
   let content = await info.json();
+  let {html_url, avatar_url, bio, name} = content;
   let title = document.createElement('h1');
   document.body.append(title);
   if (info.status !== 200) {
     title.innerHTML = 'Информация о пользователе не доступна';
   } else {
       let link = document.createElement('a');
-      link.innerHTML = content.name;
-      link.href = content.html_url;
+      link.innerHTML = name;
+      link.href = html_url;
       link.className = 'title';
       title.prepend(link);
       let photo = document.createElement('img');
-      photo.src = content.avatar_url;
+      photo.src = avatar_url;
       photo.className = 'photo';
       title.after(photo);
       let des = document.createElement('p');
-      des.innerHTML = content.bio;
+      des.innerHTML = bio;
       des.className = 'description';
       photo.after(des);
   };
 };
 
 Promise.all([today,acc])
-  .then(() => preload.classList.toggle('hide'))
+  .then(() => {
+    let preload = document.getElementById('preloader');
+    preload.classList.toggle('hide');
+  })
   .then(() => document.body.classList.toggle('body-promise'))
