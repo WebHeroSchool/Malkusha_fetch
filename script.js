f2054be8a91d1a1a31645a6686d302b32939f791
Nastya <malkusha@mail.ru>
2020-10-12 01:53:42 +0300

    promiseTry

--- script.js
+++ script.js
@@ -1,39 +1,48 @@
 let url = 'https://api.github.com/users/malkusha';
-let body = document.getElementsByTagName('body');
 let time = document.createElement('p');
 time.className = 'time';
 document.body.prepend(time);
-let preload = document.getElementById('preloader')
-let today = new Promise((resolve, reject) => {
-  setTimeout(() => resolve(time.innerHTML = new Date), 2000);
+let today = new Date();
+function getDate(today) {
+  return new Promise((resolve, reject) => {
+  setTimeout(() => resolve(today), 2000);
 });
-let acc = new Promise ((resolve, reject) => {
+};
+function getAcc() {
+  return new Promise ((resolve, reject) => {
   setTimeout(() => resolve ( getInfo()), 2000);
 });
+};
 async function getInfo() {
   let info = await fetch(url);
-  let content = await info.json();
+  let {name, html_url, avatar_url, bio} = await info.json();
   let title = document.createElement('h1');
   document.body.append(title);
   if (info.status !== 200) {
     title.innerHTML = 'Информация о пользователе не доступна';
   } else {
       let link = document.createElement('a');
-      link.innerHTML = content.name;
-      link.href = content.html_url;
+      link.innerHTML = name;
+      link.href = html_url;
       link.className = 'title';
       title.prepend(link);
       let photo = document.createElement('img');
-      photo.src = content.avatar_url;
+      photo.src = avatar_url;
       photo.className = 'photo';
       title.after(photo);
       let des = document.createElement('p');
-      des.innerHTML = content.bio;
+      des.innerHTML = bio;
       des.className = 'description';
       photo.after(des);
   };
 };
 
-Promise.all([today,acc])
-  .then(() => preload.classList.toggle('hide'))
-  .then(() => document.body.classList.toggle('body-promise'))
+Promise.all([getDate(),getAcc()])
+  .then(() => {
+    let preload = document.getElementById('preloader');
+    preload.classList.toggle('hide');
+  })
+  .then(() => {
+    document.body.classList.toggle('body-promise');
+    time.innerHTML = today;
+  })
